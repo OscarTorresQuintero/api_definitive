15a446b17a8212bd9611afd94adcc0acbdec6819
Adriana Daza <adrianaluciadazamurcia@gmail.com>
2026-03-12 07:45:34 -0500

    feat: recibir datos del héroe en la pantalla de detalle

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import '../models/superhero_model.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final hero = ModalRoute.of(context)!.settings.arguments as SuperheroModel;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hero Detail"),
+        title: Text(hero.name),
         backgroundColor: Colors.deepPurple,
       ),
-      body: const Center(
-        child: Text("Hero detail will appear here"),
+      body: Center(
+        child: Text(hero.name),
       ),
     );
   }
